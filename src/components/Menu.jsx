5ca88398641c2c988a4a105aed48cc04e2f80a9e
vibhuti-singh <vibhuti0622thakur@gmnail.com>
2023-12-08 00:36:53 +0530

    commit3rd

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Menu = () => {
   return (
     <div className='h-12 flex justify-center items-center sticky top-12 bg-white z-10 w-full'>
-  <h1 className='font-bold font-mono text-center md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl'>
+  <h1 className='font-bold font-mono text-center md:text-2xl'>
     Donut worry, be happy and eat more donuts!
   </h1>
 </div>
